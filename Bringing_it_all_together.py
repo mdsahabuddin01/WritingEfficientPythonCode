b31c6e033d9bf9a1d868d318476f5bf35d97401e
MD SAHABUDDIN <hazbun.bpl@gmail.com>
2024-12-29 23:30:22 +0000

    2_9

--- Bringing_it_all_together.py
+++ Bringing_it_all_together.py
@@ -23,7 +23,19 @@ def get_publisher_heroes_np(heroes, publishers, desired_publisher):
     return desired_heroes
 Instructions 1/4
 Use the get_publisher_heroes() function and the get_publisher_heroes_np() function to collect heroes from the Star Wars universe. The desired_publisher for Star Wars is 'George Lucas'.
+"""
+# Use get_publisher_heroes() to gather Star Wars heroes
+star_wars_heroes = get_publisher_heroes(heroes, publishers, 'George Lucas')
 
+print(star_wars_heroes)
+print(type(star_wars_heroes))
+
+# Use get_publisher_heroes_np() to gather Star Wars heroes
+star_wars_heroes_np = get_publisher_heroes_np(heroes, publishers, 'George Lucas')
+
+print(star_wars_heroes_np)
+print(type(star_wars_heroes_np))
+"""
 Question
 Within your IPython console, load the line_profiler and use %lprun to profile the two functions for line-by-line runtime. When using %lprun, use each function to gather the Star Wars heroes as you did in the previous step. After you've finished profiling, answer the following question:
 Which function has the fastest runtime?
